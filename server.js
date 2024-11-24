78b0e6c59de92cf1546aaa0fc2085f71a58d9fcb
Tami Sweitzer <tamara0715@gmail.com>
2024-11-24 13:15:51 -0600

    Test registration form. Add comments in server file . '

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require( 'express' );
 const app = express();
 
+// Tell Express we are using EJS as the templating engine.
 app.set( 'view engine', 'ejs' )
 
+// Expose the /public/ dir to the public.
+app.use( express.static( 'public' ) );
 
-app.use(express.static('public'));
+// Allows access to values from a form as 'request.body.<field>
+app.use(express.urlencoded( {extended: false} ))
 
 
 // Routes
@@ -15,6 +19,13 @@ app.get( '/', (req, res) => {
 app.get( '/login', ( req, res ) => {
   res.render( 'login' );
 })
+
+app.post( '/register', ( req, res ) => {
+  console.log(req.body);
+  res.send( "thanks" );
+})
+
+
 
 
 app.listen( 3000 );
