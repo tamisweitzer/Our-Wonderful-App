d8d608a46bc31d805ef366c551da909347788c8b
Tami Sweitzer <tamara0715@gmail.com>
2024-11-24 13:00:43 -0600

    Flesh out basic structure of home page.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require( 'express' );
 const app = express();
 
-app.set('view engine', 'ejs')
+app.set( 'view engine', 'ejs' )
 
+
+app.use(express.static('public'));
 
 app.get( '/', (req, res) => {
   res.render('homepage');
