caa918c822fcc6237f9f4d6da473f1a321f7b66f
Tami Sweitzer <tamara0715@gmail.com>
2024-11-24 12:50:24 -0600

    Add EJS package for templating.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require( 'express' );
 const app = express();
 
-
+app.set('view engine', 'ejs')
 
 
 app.get( '/', (req, res) => {
-  res.send( "Hello world" );
+  res.render('homepage');
 });
 
 
