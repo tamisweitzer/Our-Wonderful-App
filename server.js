de9859c66b3c3e6bf54c872e6fd0a3f187a5d40b
Tami Sweitzer <tamara0715@gmail.com>
2024-11-24 20:37:21 -0600

    Formatting. Add blank lines between sections. Wrap one line code in curly braces.

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const bcrypt = require( 'bcrypt' );
 const jwt = require( 'jsonwebtoken' );
 const cookieParser = require( 'cookie-parser' );
 
-
 db.pragma('journal_mode = WAL')
 
-// Database set up
+
+// Create tables for our database.
 const createTables = db.transaction( () => {
   db
     .prepare( `
@@ -22,10 +22,12 @@ const createTables = db.transaction( () => {
       `)
     .run()
 });
-
 createTables();
-// end
 
+
+
+
+// Middleware_______________________
 
 // Tell Express we are using EJS as the templating engine.
 app.set( 'view engine', 'ejs' )
@@ -38,7 +40,7 @@ app.use( express.urlencoded( { extended: false } ) );
 
 app.use( cookieParser() );
 
-// Middleware to set the errors array to an empty array so that we don't get an error when we first load the page.
+// Set the errors array to an empty array so that we don't get an error when we first load the page.
 app.use( function ( req, res, next ) {
   res.locals.errors = [];
   // Try to decode incoming cookie to determine if user is logged in.
@@ -48,12 +50,17 @@ app.use( function ( req, res, next ) {
   } catch (error) {
     req.user = false;
   }
-
   res.locals.user = req.user;
-  console.log( req.user);
-
   next();
 } );
+
+// Ensure visitor is logged in.
+function mustBeLoggedIn( req, res, next ) {
+  if ( req.user ) {
+    next();
+  }
+  return res.redirect( '/' );
+}
 
 
 
@@ -81,14 +88,17 @@ app.get( '/logout', ( req, res ) => {
 } );
 
 // GET '/dashboard' Redirect logged in users to their dashboard.
-app.get( '/dashboard', ( req, res ) => {
+app.get( '/dashboard', mustBeLoggedIn, ( req, res ) => {
   res.render( 'dashboard' );
 } );
 
 // GET '/create-post' Navigate to the Create Post page.
-app.get( '/create-post', ( req, res ) => {
+app.get( '/create-post', mustBeLoggedIn, ( req, res ) => {
   res.render( 'create-post' );
 })
+
+
+
 
 // POST Requests_______________________
 
@@ -97,38 +107,37 @@ app.post( '/register', ( req, res ) => {
 const errors = [];
 
    // Check if we're getting strings back from the form.
-  if ( typeof req.body.username !== 'string' ) req.body.username = '';
-  if ( typeof req.body.password !== 'string' ) req.body.password = '';
+  if ( typeof req.body.username !== 'string' ) { req.body.username = ''; }
+  if ( typeof req.body.password !== 'string' ) { req.body.password = ''; }
 
   // Trim any whitespace from the form before validating input.
   req.body.username = req.body.username.trim();
   req.body.password = req.body.password.trim();
 
   // Validate username is minimum 3 chars, and maximum 13 chars.
-  if ( !req.body.username ) errors.push( "You must provide a username." );
-  if ( req.body.username && req.body.username.length < 3 ) errors.push( "Username must be at least 3 characters long." );
-  if ( req.body.username && req.body.username.length > 13 ) errors.push( "Username must be less than 14 characters long." );
+  if ( !req.body.username ) { errors.push( "You must provide a username." ); }
+  if ( req.body.username && req.body.username.length < 3 ) { errors.push( "Username must be at least 3 characters long." ); }
+  if ( req.body.username && req.body.username.length > 13 ) { errors.push( "Username must be less than 14 characters long." ); }
 
   // Disallow special characters.
-  if ( req.body.username && !req.body.username.match( /^[a-zA-Z0-9]+$/ ) ) errors.push( "Username may not contain special characters." );
+  if ( req.body.username && !req.body.username.match( /^[a-zA-Z0-9]+$/ ) ) { errors.push( "Username may not contain special characters." ); }
 
   // Check if username exists already.
   const usernameExistsStmt = db.prepare( "SELECT * FROM users WHERE username = ?" );
   const usernameExists = usernameExistsStmt.get( req.body.username );
-  if ( usernameExists ) errors.push( "Please choose a different username" );
+
+  if ( usernameExists ) { errors.push( "Please choose a different username" ); }
 
   // Validate password is minimum 3 chars, and maximum 13 chars.
-  if ( !req.body.password ) errors.push( "You must provide a password." );
-  if ( req.body.password && req.body.password.length < 8 ) errors.push( "Password must be at least 8 characters long." );
-  if ( req.body.password && req.body.password.length > 100 ) errors.push( "Password must be less than 101 characters long." );
+  if ( !req.body.password ) { errors.push( "You must provide a password." ); }
+  if ( req.body.password && req.body.password.length < 8 ) { errors.push( "Password must be at least 8 characters long." ); }
+  if ( req.body.password && req.body.password.length > 100 ) { errors.push( "Password must be less than 101 characters long." ); }
 
   // Disallow special characters.
-  if ( req.body.password && !req.body.password.match( /^[a-zA-Z0-9]+$/ ) ) errors.push( "Password may not contain special characters." );
+  if ( req.body.password && !req.body.password.match( /^[a-zA-Z0-9]+$/ ) ) { errors.push( "Password may not contain special characters." ); }
 
   // If there are any errors, redirect to home page and display errors.
-  if ( errors.length ) {
-    return res.render( 'homepage', { errors } );
-  }
+  if ( errors.length ) { return res.render( 'homepage', { errors } );  }
 
   // Encrypt the password before sending to the database.
   const salt = bcrypt.genSaltSync( 10 );
@@ -166,11 +175,12 @@ const errors = [];
   res.redirect( '/' );
 } );
 
+
 // POST '/login' Process login request
 app.post( '/login', ( req, res ) => {
   const errors = [];
-  if ( typeof req.body.username !== 'string' ) req.body.username = '';
-  if ( typeof req.body.password !== 'string' ) req.body.password = '';
+  if ( typeof req.body.username !== 'string' ) { req.body.username = ''; }
+  if ( typeof req.body.password !== 'string' ) { req.body.password = ''; }
 
   if ( req.body.username.trim() === "" || req.body.password === "") {
     errors.push( "Invalid username or password" );
@@ -210,9 +220,16 @@ app.post( '/login', ( req, res ) => {
   } );
 
   res.redirect( '/' );
+} );
 
 
-});
+// POST '/create-post' Process attempt to create a post.
+app.post( '/create-post', mustBeLoggedIn, ( req, res ) => {
+  console.log(req);
+  res.send( "Thanks" );
+})
+
+
 
 
 
